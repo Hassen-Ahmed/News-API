b1f326d68a60b2d373166a1dbe226186cf0b8a70
Hassen-Ahmed <hassenbest23@gmail.com>
2023-06-26 11:00:59 +0100

    initial commit and project setup

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Northcoders News API
 
-For instructions, please head over to [L2C NC News](https://l2c.northcoders.com/courses/be/nc-news).
+Please create Two .env at top level which are .env.development and .env.test.
+And also don't forget to include you database name i.e PGDATABASE=someDatabaseName
+
+In addition I included my psql password into .env files i.e PG_PASSWORD=yourPassword
--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,14 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
+require("dotenv").config({
+    path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+    throw new Error("PGDATABASE not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool({
+    password: process.env.PG_PASSWORD,
+});
