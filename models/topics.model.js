c1a0e69989fe3e78d70996b0d286f4828fdc7ce3
Hassen-Ahmed <hassenbest23@gmail.com>
2023-06-26 12:21:03 +0100

    core-1 updated by add catch block

--- models/topics.model.js
+++ models/topics.model.js
@@ -3,9 +3,14 @@ const db = require("../db/connection");
 function selectAllTopics() {
     const query = "SELECT * FROM topics";
 
-    return db.query(query).then(({ rows }) => {
-        return rows;
-    });
+    return db
+        .query(query)
+        .then(({ rows }) => {
+            return rows;
+        })
+        .catch((err) => {
+            return Promise.reject({ status: 404, msg: "Not found" });
+        });
 }
 
 module.exports = { selectAllTopics };
