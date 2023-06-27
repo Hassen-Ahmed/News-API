c70eca22e77c1adf10458e3c01e0afff6d4bdc37
Hassen-Ahmed <hassenbest23@gmail.com>
2023-06-27 11:17:38 +0100

    4-ticket is done

---
+++ controllers/articles.controller.js
@@ -0,0 +1,11 @@
+const { selectArticlesById } = require("../models/articles.model");
+
+exports.getArticlesById = (req, res, next) => {
+    const { article_id } = req.params;
+
+    return selectArticlesById(article_id)
+        .then((articles) => {
+            res.status(200).send({ articles });
+        })
+        .catch(next);
+};
