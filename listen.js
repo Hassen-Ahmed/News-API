e4202951e37cb779843b5a5f17ec3fb4912b668e
Hassen-Ahmed <hassenbest23@gmail.com>
2023-06-29 12:47:01 +0100

    server setting up

--- listen.js
+++ listen.js
@@ -3,5 +3,5 @@ const app = require("./app");
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => {
-    console.log("Server running on port 9090....");
+    console.log(`Server running on port ${PORT}....`);
 });
