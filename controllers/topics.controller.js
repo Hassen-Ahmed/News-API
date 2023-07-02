b8ad2c22a039522ffe7607d6395f30ca3d697f85
Hassen-Ahmed <hassenbest23@gmail.com>
2023-07-02 16:47:50 +0100

    21-ticket-advance is done with TDD

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,7 +1,16 @@
-const { selectAllTopics } = require("../models/topics.model");
+const { selectAllTopics, insertNewTopic } = require("../models/topics.model");
 
 exports.getAllTopics = (_, res) => {
     return selectAllTopics().then((topics) => {
         res.status(200).send({ topics });
     });
+};
+
+exports.postNewTopic = (req, res, next) => {
+    const { slug, description } = req.body;
+    insertNewTopic(slug, description)
+        .then((topic) => {
+            res.status(201).send({ topic });
+        })
+        .catch(next);
 };
